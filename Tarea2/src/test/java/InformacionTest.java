a6f7a328aca714e17bc7dd775cff0a2e084b0905
Chloe Yáñez <agyanez2023@udec.cl>
2024-05-12 01:48:48 -0400

    Creación Test Informacion

---
+++ Tarea2/src/test/java/InformacionTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InformacionTest {
+
+    @Test
+    void cnvAsistenciaAEmpleados() {
+    }
+
+    @Test
+    void cnvInvitadosAEmpleados() {
+    }
+
+    @Test
+    void printNombresAsistentes() {
+    }
+
+    @Test
+    void printNombresInvitados() {
+    }
+
+    @Test
+    void printRegistroReunion() {
+    }
+}
