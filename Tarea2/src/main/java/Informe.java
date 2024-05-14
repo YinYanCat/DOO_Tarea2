07f510721cd2af69c1ee7af1f2b22ee44b7c0ff9
Emyowo204 <egaete2023@inf.udec.cl>
2024-05-14 17:08:51 -0400

    Documentación de la clase Informe

--- Tarea2/src/main/java/Informe.java
+++ Tarea2/src/main/java/Informe.java
@@ -7,7 +7,15 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/** Un informe que contiene información sobre una reunión, no contiene constructor
+ * @author Chloe Yañez Lavin
+ * @author Emily Osvaldo Gaete Bobadilla */
+
 public class Informe {
+
+    /** Método para convertir una lista de asistencias en un String de nombres de los empleados y horas
+     * @param listaAsistencia La lista de asistencias que contienen empleados
+     * @return El String de los nombres y las horas guardadas en las asistencias, separadas por líneas  */
     public String listaAsistenciaNombres(ArrayList<Asistencia> listaAsistencia) {
         String listaEmpleados = "";
         for (int i = 0; i < listaAsistencia.size(); i++) {
@@ -17,6 +25,10 @@ public class Informe {
         }
         return listaEmpleados;
     }
+
+    /** Método para convertir una lista de invitaciones en un String de nombres de los empleados y horas
+     * @param listaInvitados La lista de invitaciones que contienen empleados
+     * @return El String de los nombres y las horas guardadas en las invitaciones, separadas por líneas */
     public String listaInvitadosNombres(ArrayList<Invitacion> listaInvitados) {
         String listaEmpleados = "";
         for (int i = 0; i < listaInvitados.size(); i++) {
@@ -27,6 +39,9 @@ public class Informe {
         return listaEmpleados;
     }
 
+    /** Método para convertir un instante en un String con la hora entregada en la zona horaria de Chile
+     * @param hora El instante con la hora a convertir
+     * @return El String con la hora cambiada de la zona horaria UTC a CLT */
     public String toStringHoraCLT(Instant hora) {
         LocalDateTime now = LocalDateTime.now();
         ZoneId zone = ZoneId.of("America/Santiago");
@@ -34,21 +49,23 @@ public class Informe {
         return hora.atZone(zoneOffSet).toLocalTime().toString();
     }
 
-    public void crearArchivoTXT(String nombre) {
-        try {
-            File myObj = new File(nombre+".txt");
-            if (myObj.createNewFile()) {
-                System.out.println("Archivo creado: "+myObj.getName());
-            } else {
-                System.out.println("Archivo ya existente");
-            }
-        } catch (IOException e) {
-            System.out.println("Error al crear archivo.txt");
-            e.printStackTrace();
+    /** Método para crear un archivo de texto a partir de un nombre
+     * @param nombre El nombre del archivo de texto, el propio método agrega ".txt" al nombre
+     * @throws IOException Se lanza esta excepción si hay un error al crear el archivo de texto */
+    public void crearArchivoTXT(String nombre) throws IOException {
+        File myObj = new File(nombre+".txt");
+        if (myObj.createNewFile()) {
+            System.out.println("Archivo creado: "+myObj.getName());
+        } else {
+            System.out.println("Archivo "+myObj.getName()+" ya existente");
         }
     }
 
-    public void crearInformeReunion(Reunion reunion, String nombre) {
+    /** Método para crear un archivo de texto a partir de un nombre
+     * @param reunion La reunión de donde se obtendrá la información para crear el informe
+     * @param nombre El nombre del archivo de texto, al crear el archivo el método agrega ".txt" al nombre
+     * @throws IOException Se lanza esta excepción si hay un error al crear el archivo de texto */
+    public void crearInformeReunion(Reunion reunion, String nombre) throws IOException {
         crearArchivoTXT(nombre);
         try {
             String sFecha = "Fecha: " + reunion.getFecha().toString();
@@ -68,7 +85,7 @@ public class Informe {
             myWriter.write("Tipo de Reunión: " + reunion.getTipo()+"\n"+sOrg+"\n"+reunion.getLugar()+"\n\n");
             myWriter.write(sAsis+"\n\n"+sRet+"\n\n"+sAus+"\n\n");
             myWriter.write("Asistencia Total: "+reunion.obtenerTotalAsistencia()+"/"+reunion.obtenerTotalInvitacion());
-            myWriter.write(" // "+reunion.obtenerPorcentajeAsistencia()+"%\n\n");
+            myWriter.write(" | "+reunion.obtenerPorcentajeAsistencia()+"%\n\n");
             myWriter.write("Notas de la Reunión:"+reunion.getNota().getContenido());
             myWriter.close();
             System.out.println("Informe de la reunión creado en: "+nombre+".txt");
